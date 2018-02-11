23b9918d3f0050b83fb7a9e717266e86afe59e74
Ricardo Frade <ricardojfrade@gmail.com>
2018-02-11 19:05:34 +0000

    add rx

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,11 +4,8 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_app/MoviesApp.dart';
-
-import 'package:test_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
